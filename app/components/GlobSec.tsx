525de2b91bb73ac478ed90b8af263a78bd1bd12f
Ahmed Esmail <ahmedp30895533@pharm.tanta.edu.eg>
2025-06-07 19:43:57 +0300

    style: disable text selection and adjust layout spacing
    - Add `user-select: none` to html element to prevent text selection
    - Adjust padding and positioning of elements in GlobSec component for better visual consistency

--- app/components/GlobSec.tsx
+++ app/components/GlobSec.tsx
@@ -399,7 +399,7 @@ const GlobSec = () => {
   ];
 
   return (
-    <div className="flex flex-row items-center justify-center py-0 h-screen md:h-auto  relative w-full">
+    <div className="flex flex-row items-center justify-center py-20 h-screen md:h-auto  relative w-full">
       <div className="max-w-7xl mx-auto w-full relative overflow-hidden h-full md:h-[40rem] px-4">
         <motion.div
           initial={{
@@ -424,7 +424,7 @@ const GlobSec = () => {
           </p>
         </motion.div>
         {/* <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-black to-white z-40" /> */}
-        <div className="absolute w-full -bottom-20 h-72 md:h-full z-10">
+        <div className="absolute w-full -bottom-5 lg:-bottom-20 h-72  md:h-full z-10">
           <World data={sampleArcs} globeConfig={globeConfig} />
         </div>
       </div>
